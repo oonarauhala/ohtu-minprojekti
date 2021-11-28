dbe88de78377a90b91c5edcef6c907858bfda84f
Joona Kostamo <joona.kostamo@outlook.com>
2021-11-29 01:37:24 +0200

    Minimal db integration

--- src/app.py
+++ src/app.py
@@ -1,9 +1,24 @@
-from flask import Flask
+from flask import Flask, render_template
+import sqlite3 as sql
 
 
-app = Flask(__name__)
+app = Flask(__name__,template_folder='templates')
 
+@app.route('/list')
+def list():
+   con = sql.connect("tietokanta.db")
+   con.row_factory = sql.Row
+   
+   cur = con.cursor()
+   cur.execute("select * from Vinkit")
+   
+   rows = cur.fetchall(); 
+   return render_template("list.html",rows = rows)
+
+   
 
 @app.route("/")
 def index():
-    return "Moi"
+   return render_template("index.html")
+
+app.run(debug = True)
