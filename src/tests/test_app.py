45153c3f544b905e8744005c7719f8f2d4020ecb
Maija Pajumaa <maija.pajumaa@helsinki.fi>
2021-12-05 18:22:57 +0200

    Added test setup and first test

--- src/tests/test_app.py
+++ src/tests/test_app.py
@@ -1,7 +1,15 @@
 import unittest
 from flask import Flask
 import sqlite3 as sql
+from app import app
 
 class TestApp(unittest.TestCase):
-    def test_eka_testi(self):
-        self.assertEqual(1, 1)
+    def setUp(self):
+        app.config['TESTING']=True
+        app.config['WTF_CSRF_ENABLED'] = False
+        app.config['DEBUG'] = False
+        self.app = app.test_client()
+
+    def test_index_page(self):
+        response = self.app.get("/")
+        self.assertEqual(response.status_code, 200)
