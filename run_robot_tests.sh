1d2bbb78064a6fb1e8bea69765f5f139cc27e0bc
Oona Rauhala <oona.e.rauhala@gmail.com>
2021-12-08 20:04:39 +0200

    Update robot test script

--- run_robot_tests.sh
+++ run_robot_tests.sh
@@ -1,21 +1,18 @@
 #!/bin/bash
 
-# käynnistetään Flask-palvelin taustalle (huomaa & komennon lopussa)
+# käynnistetään flask-palvelin taustalle
 poetry run python3 src/index.py &
 
-# odetetaan, että palvelin on valmiina ottamaan vastaan pyyntöjä,
-# jolloin localhost:5000/ping antaa vastauksen statuskoodilla 200
-while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' localhost:5000/)" != "200" ]]; 
+# odetetaan, että palvelin on valmiina ottamaan vastaan pyyntöjä
+while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' localhost:5000/ping)" != "200" ]]; 
   do sleep 1; 
 done
 
 # suoritetaan testit
-poetry run robot src/tests/robot
+poetry run robot src/e2e
 
-# pysäytetään Flask-palvelin portissa 5000
-function clean_up {
-  kill $(lsof -t -i:5000)
-}
+status=$?
 
-# suoritetaan clean_up-funktio, kun prosessi lopettaa suorituksen
-trap clean_up EXIT
+kill $(lsof -t -i:5000)
+
+exit $status
