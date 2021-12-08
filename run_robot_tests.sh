062c92a83e900087f0bd0afc2596ca9a18fee80a
Oona Rauhala <oona.e.rauhala@gmail.com>
2021-12-08 20:13:43 +0200

    Update robot test script

--- run_robot_tests.sh
+++ run_robot_tests.sh
@@ -4,7 +4,7 @@
 poetry run python3 src/index.py &
 
 # odetetaan, että palvelin on valmiina ottamaan vastaan pyyntöjä
-while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' localhost:5000/ping)" != "200" ]]; 
+while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' localhost:5000/)" != "200" ]]; 
   do sleep 1; 
 done
 
